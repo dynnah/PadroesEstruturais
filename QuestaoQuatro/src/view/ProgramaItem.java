161a542a772e70ec74d75fd0acc75645d6128adf
dynnah <dynnah_hanna@hotmail.com>
2019-11-22 22:52:27 -0300

    Update ProgramaItem.java

--- QuestaoQuatro/src/view/ProgramaItem.java
+++ QuestaoQuatro/src/view/ProgramaItem.java
@@ -9,11 +9,13 @@ public class ProgramaItem {
 		Pasta p1 = new Pasta("Pasta 1");
 		Pasta p2 = new Pasta("Pasta 2");
 		Pasta p3 = new Pasta("Pasta 3");
-		Arquivo a1 = new Arquivo("Arquivo 1", 15);
-		Arquivo a2 = new Arquivo("Arquivo 2", 30);
-		Arquivo a3 = new Arquivo("Arquivo 3", 60);
-		Arquivo a4 = new Arquivo("Arquivo 4", 120);
-		Arquivo a5 = new Arquivo("Arquivo 5", 240);
+		Pasta p4 = new Pasta("Pasta 4");
+		Arquivo a1 = new Arquivo("Arquivo 1", 5);
+		Arquivo a2 = new Arquivo("Arquivo 2", 10);
+		Arquivo a3 = new Arquivo("Arquivo 3", 15);
+		Arquivo a4 = new Arquivo("Arquivo 4", 20);
+		Arquivo a5 = new Arquivo("Arquivo 5", 25);
+		Arquivo a6 = new Arquivo("Arquivo 6", 30);
 
 		p1.inserir(a1);
 		p1.inserir(p2);
@@ -22,11 +24,14 @@ public class ProgramaItem {
 		p2.inserir(p3);
 		p3.inserir(a4);
 		p3.inserir(a5);
+		p3.inserir(p4);
+		p4.inserior(a6);
 		
 		
 		p1.exibir();
 		//p2.exibir();
 		//p3.exibir();
+		//p4.exibir();
 		
 	}
 
